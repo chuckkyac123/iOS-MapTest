f3097f49619d155fb7d9036f91eadee442851334
chuckkyac123 <chuckkyac123@gmail.com>
2015-06-05 20:59:10 +0700

    Initial Commit

---
+++ MapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapTest
+//
+//  Created by Chuckky Bank on 6/5/15.
+//  Copyright (c) 2015 Chuckky Bank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MapTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapTest
+//
+//  Created by Chuckky Bank on 6/5/15.
+//  Copyright (c) 2015 Chuckky Bank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
